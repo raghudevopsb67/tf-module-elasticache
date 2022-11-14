31bdaa0820313525a0364956d2dd536fe39474fd
Raghu K <raghuk.vit@gmail.com>
2022-11-14 06:48:55 +0530

    Update tf-module-elasticache/

--- vars.tf
+++ vars.tf
@@ -1,2 +1,8 @@
 variable "elasticache" {}
 variable "env" {}
+variable "name" {}
+variable "engine" {}
+variable "engine_version" {}
+variable "node_type" {}
+variable "num_cache_nodes" {}
+variable "subnets" {}
